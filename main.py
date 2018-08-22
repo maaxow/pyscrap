ca557bbb057d50ad9e77fff32e81fb82db5a3330
Max <maxime.rose@hotmail.fr>
2018-08-22 23:41:20 +0200

    init 2

--- main.py
+++ main.py
@@ -1,39 +1,59 @@
 import urllib.request
+import json
 from bs4 import BeautifulSoup
 
-url = 'https://www.lesbonsnumeros.com/loto/resultats/tirages-decembre-2008.htm'
-template = 'tirages-{{mois}}-{{annee}}.htm'
+#url = 'https://www.lesbonsnumeros.com/loto/resultats/tirages-janvier-2009.htm'
+url = 'http://www.tirage-euromillions.net/loto/annee/tirages-2008/'
+template = 'tirages-{mois}-{annee}.htm'
+#print(template.format(mois='decembre', annee='2008'))
 tirage = {
     'date': '',
     'numeros': [],
     'chance': 0
 }
-def initTirage():
-    tirage['date'] = ''
-    tirage['chance'] = 0
-    tirage['numeros'] = []
+f = open('data.json', 'r+')
 
 def extractTable():
     page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
+    soup = BeautifulSoup(page, 'html5lib')
     monthlyTirage = []
-    lines = soup.find_all('ul', attrs={'class': 'tirage tirageSmall loto'})
-    for line in lines:
-        boules = line.find_all('li', attrs={'class': 'boule'})
-        chance = line.find('li', attrs={'class': 'chance'})
-        tirage['chance'] = chance.text
-        tirage['numeros'].append(boules[0].text)
-        tirage['numeros'].append(boules[1].text)
-        tirage['numeros'].append(boules[2].text)
-        tirage['numeros'].append(boules[3].text)
-        tirage['numeros'].append(boules[4].text)
-        print(tirage)
+    table = soup.find('table', id='tiragesAnneeLoto')
+    tbody = table.find('tbody')
+    rows = tbody.find_all('tr')
+    #file = open('page.html', 'r+')
+    #file.write('')
+    #file.write(str(trs))
+    #file.close()
+    #print(str(trs))
+    file = open('page.html', 'r+')
+    file.write('')
+    for line in rows:
+        tds = line.find_all('td')
+        tirage = {
+            'date': '',
+            'numeros': [],
+            'chance': 0
+        }
+        if len(tds) == 9:
+            date = tds[0].a.text
+            tirage['date'] = tds[0].a.text
+            file.write(str(date))
+            file.write('\n')
+
+        #tirage['chance'] = chance.text
+        #tirage['numeros'].append(boules[0].text)
+        #tirage['numeros'].append(boules[1].text)
+        #tirage['numeros'].append(boules[2].text)
+        #tirage['numeros'].append(boules[3].text)
+        #tirage['numeros'].append(boules[4].text)
         monthlyTirage.append(tirage)
-        initTirage()
+        
+    json.dump(monthlyTirage, f)
+    file.close()
     return monthlyTirage
 
-print(extractTable())
-
+extractTable()
+f.close()
 
 
 
