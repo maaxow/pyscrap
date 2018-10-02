49d4280cbc89ca7bb40f53475ec87ed2addf79f1
rosemx <maxime.rose@cgi.com>
2018-10-02 09:26:30 +0200

    add proxy switcher

--- main.py
+++ main.py
@@ -8,32 +8,37 @@ tirage = {
     'numeros': [],
     'chance': 0
 }
+useProxy = 0 # 0 = true, 1 = false
+proxies = {
+	'http': 'http://fr-proxy.groupinfra.com:3128',
+	'https': 'http://fr-proxy.groupinfra.com:3128'
+}
 def initTirage():
     tirage['date'] = ''
     tirage['chance'] = 0
     tirage['numeros'] = []
 
 def extractTable():
-    page = urllib.request.urlopen(url)
-    soup = BeautifulSoup(page, 'html.parser')
-    monthlyTirage = []
-    lines = soup.find_all('ul', attrs={'class': 'tirage tirageSmall loto'})
-    for line in lines:
-        boules = line.find_all('li', attrs={'class': 'boule'})
-        chance = line.find('li', attrs={'class': 'chance'})
-        tirage['chance'] = chance.text
-        tirage['numeros'].append(boules[0].text)
-        tirage['numeros'].append(boules[1].text)
-        tirage['numeros'].append(boules[2].text)
-        tirage['numeros'].append(boules[3].text)
-        tirage['numeros'].append(boules[4].text)
-        print(tirage)
-        monthlyTirage.append(tirage)
-        initTirage()
-    return monthlyTirage
+	if useProxy == 0:
+		urllib.request.ProxyHandler(proxies=proxies)
+	else:
+		urllib.request.ProxyHandler(proxies=None)
+	page = urllib.request.urlopen(url)
+	soup = BeautifulSoup(page, 'html.parser')
+	monthlyTirage = []
+	lines = soup.find_all('ul', attrs={'class': 'tirage tirageSmall loto'})
+	for line in lines:
+		boules = line.find_all('li', attrs={'class': 'boule'})
+		chance = line.find('li', attrs={'class': 'chance'})
+		tirage['chance'] = chance.text
+		tirage['numeros'].append(boules[0].text)
+		tirage['numeros'].append(boules[1].text)
+		tirage['numeros'].append(boules[2].text)
+		tirage['numeros'].append(boules[3].text)
+		tirage['numeros'].append(boules[4].text)
+		print(tirage)
+		monthlyTirage.append(tirage)
+		initTirage()
+	return monthlyTirage
 
 print(extractTable())
-
-
-
-
